afc847075ea1f1d564e701be9f4899d2cda5148c
Alan Dias <jusbrasil@JUS-202300505.local>
2024-04-06 22:19:13 -0300

    feat: adding overall progress bar and progress value to each indicador

--- components/CreateWorkoutButton.tsx
+++ components/CreateWorkoutButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from './ui/button';
+import { BsStars } from "react-icons/bs";
 
 type CreateWorkoutButtonProps = {
     onClick: () => void; // This specifies that onClick is a function that takes no arguments and doesn't return anything
@@ -8,7 +9,10 @@ type CreateWorkoutButtonProps = {
 
 const CreateWorkoutButton = ({ onClick }:CreateWorkoutButtonProps) => {
   return (
-    <Button className="max-w-60" onClick={onClick}>Create Workout</Button>
+    <Button className="max-w-60" onClick={onClick}>
+      Create Workout
+      <BsStars className="mx-2" />
+    </Button>
   );
 };
 
